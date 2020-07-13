be9daec2b49104d85255e57d7565b2abd65e2b24
JS GREWAL <jsgrewal1204@outlook.com>
2020-07-13 17:43:38 +0530

    a

--- Server.js
+++ Server.js
@@ -1,4 +1,4 @@
-"use strict";
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -35,7 +35,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-exports.__esModule = true;
+
 var dbConnector = require("./EraDBConnect");
 // let dbConnector = require('./EraDBCon')
 var express = require('express');
